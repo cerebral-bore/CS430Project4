6652d9c8657fcdce2241f1f52965209aef7d616c
jjg297 <garciajj730@gmail.com>
2016-11-15 21:58:59 -0700

    Update raytracer.c
    Finalized formatting

--- raytracer.c
+++ raytracer.c
@@ -18,51 +18,47 @@
 // Struct and function declarations
 
 typedef struct Object{
-  int kind; // 1,2,3,4 = sphere, plane, camera, light respectively
-  double diffuse_color[3];
-  double specular_color[3];
-  double reflectivity;
-  double refractivity;
-  double ior;
-  union {
-    struct {
-      double center[3];
-      double radius;
-    } sphere;
-    struct {
-      double center[3];
-      double width;
-      double height;
-      double normal[3];
-    } plane;
-    struct {
-      double width;
-      double height;
-    } camera;
-    struct light{
-      double center[3];
-      double color[3];
-      double direction[3];
-      double theta;
-      double radial_a2;
-      double radial_a1;
-      double radial_a0;
-      double angular_a0;
-    } light;
-  };
+	int kind; // 1,2,3,4 = sphere, plane, camera, light respectively
+	double diffuse_color[3];
+	double specular_color[3];
+	double reflectivity;
+	double refractivity;
+	double ior;
+	union {
+		struct {
+			double center[3];
+			double radius;
+		} sphere;
+		struct {
+			double center[3];
+			double width;
+			double height;
+			double normal[3];
+		} plane;
+		struct {
+			double width;
+			double height;
+		} camera;
+		struct light{
+			double center[3];
+			double color[3];
+			double direction[3];
+			double theta;
+			double radial_a2;
+			double radial_a1;
+			double radial_a0;
+			double angular_a0;
+		} light;
+	};
 } Object;
 
-typedef struct dPixel{
-  double r,g,b;
-} dPixel;
-
 typedef struct Pixel{
-  unsigned char r,g,b;
+	unsigned char r,g,b;
 } Pixel;
 
 typedef Object Scene[128];
 int read_scene(char* json, Scene scene);
-int raycast (Pixel *buffer, Scene scene, int num_objects, int width, int height);
+int raytrace (Pixel *buffer, Scene scene, int num_objects, int width, int height);
 int ppm_output(Pixel  *buffer, char *output_file_name, int size, int depth, int width, int height);
 
 double* next_vector(FILE* json);
@@ -71,11 +67,12 @@ char* next_string(FILE* json);
 void skip_ws(FILE* json);
 void expect_c(FILE* json, int d);
 int next_c(FILE* json);
+int errCheck(int argc, char *argv[]);
 
 double sphere_intersection(double* Ro, double* Rd, double* C, double r);
 double plane_intersection(double* Ro, double* Rd, double* c, double *n);
 static inline double sqr(double v) {
-  return v*v;
+	return v*v;
 }
 
 void shoot_ray(Object *scene, int num_objects, double *Ro, double *Rd, double *t, int *o);
@@ -86,104 +83,101 @@ void shade_rec(Object *sceneRef, int num_objects, double best_t, int best_obj, d
 typedef double* V3;
 
 static inline void v3_add(V3 a, V3 b, V3 c){
-  c[0] = a[0] + b[0];
-  c[1] = a[1] + b[1];
-  c[2] = a[2] + b[2];
+	c[0] = a[0] + b[0];
+	c[1] = a[1] + b[1];
+	c[2] = a[2] + b[2];
 }
 
 static inline void v3_sub(V3 a, V3 b, V3 c){
-  c[0] = a[0] - b[0];
-  c[1] = a[1] - b[1];
-  c[2] = a[2] - b[2];
+	c[0] = a[0] - b[0];
+	c[1] = a[1] - b[1];
+	c[2] = a[2] - b[2];
 }
 
 static inline void v3_scale(V3 a, double s, V3 c){
-  c[0] = s * a[0];
-  c[1] = s * a[1];
-  c[2] = s * a[2];  
+	c[0] = s * a[0];
+	c[1] = s * a[1];
+	c[2] = s * a[2];  
 }
 
 static inline double v3_dot(V3 a, V3 b){
-  return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
 static inline void v3_cross(V3 a, V3 b, V3 c){
-  c[0] = a[1] * b[2] - a[2] * b[1];
-  c[1] = a[2] * b[0] - a[0] * b[2];
-  c[2] = a[0] * b[1] - a[1] * b[0];
+	c[0] = a[1] * b[2] - a[2] * b[1];
+	c[1] = a[2] * b[0] - a[0] * b[2];
+	c[2] = a[0] * b[1] - a[1] * b[0];
 }
 
 static inline void v3_normalize(double* v) {
-  double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
-  v[0] /= len;
-  v[1] /= len;
-  v[2] /= len;
+	double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
+	v[0] /= len;
+	v[1] /= len;
+	v[2] /= len;
 }
 
 static inline void v3_reflect(V3 A, V3 N, V3 C){
-  double q = 2*v3_dot(A, N);
-  double rscale[3];
-  v3_scale(N, q, rscale);
-  v3_sub(A, rscale, C);
-  v3_normalize(C);
+	double q = 2*v3_dot(A, N);
+	double rscale[3];
+	v3_scale(N, q, rscale);
+	v3_sub(A, rscale, C);
+	v3_normalize(C);
 }
 
 static inline double v3_mag(double* v){
-  double d = (double)sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
-  return d;
+	double d = (double)sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
+	return d;
 }
 
 static inline int clamp(double d){
-  int final;
-  if (d > 255.0) final = 255;
-  else if (d < 0.0) final = 0;
-  else final = floor(d);
-  return final;
+	int final;
+	if (d > 255.0){ final = 255; }
+	else if (d < 0.0){ final = 0; }
+	else{ final = floor(d); }
+	return final;
 }
 
 static inline double distance(V3 Ro, V3 p){
-  return sqrt(sqr(Ro[0] - p[0]) + sqr(Ro[1] - p[1]) + sqr(Ro[2] - p[2]));
+	return sqrt(sqr(Ro[0] - p[0]) + sqr(Ro[1] - p[1]) + sqr(Ro[2] - p[2]));
 }
 
 static inline double frad(double a0, double a1, double a2, double dl){
-  return 1/(a2*sqr(dl) + a1*dl + a0);
+	return 1/(a2*sqr(dl) + a1*dl + a0);
 }
 
 static inline double fang(V3 Rd, V3 Ld, double theta, double a0){
-  v3_normalize(Rd);
-  v3_normalize(Ld);
-  if(theta == 0)  
-    return 1;
-  double l_theta = v3_dot(Ld, Rd);
-  if (acos(l_theta) > theta)
-    return 0;
-  else{
-    return pow(l_theta, a0);
-  }
+	v3_normalize(Rd);
+	v3_normalize(Ld);
+	if(theta == 0){ return 1; }
+	double l_theta = v3_dot(Ld, Rd);
+	if(acos(l_theta) > theta){
+		return 0;
+	} else{
+		return pow(l_theta, a0);
+	}
 }
-
-int errCheck(int argc, char *argv[]);
 
 int main(int argc, char* argv[]){
   
-  errCheck(argc, argv);
+	errCheck(argc, argv);
 
-  // Initialize scene
-  Object *scene = malloc(sizeof(Object)*128);
+	// Initialize scene
+	Object *scene = malloc(sizeof(Object)*128);
 
-  // Populate scene by parsing json
-  int num_objects = read_scene(argv[3], scene);
+	// Populate scene by parsing json
+	int num_objects = read_scene(argv[3], scene);
 
-  // Create the pixel buffer and populate it
-  // Store the desired picture height and width
-  int width = atoi(argv[1]);
-  int height = atoi(argv[2]);
-  Pixel pixbuffer[width*height];
-  raycast(pixbuffer, scene, num_objects, width, height);
+	// Create the pixel buffer and populate it
+	// Store the desired picture height and width
+	int width = atoi(argv[1]);
+	int height = atoi(argv[2]);
+	Pixel pixbuffer[width*height];
+	raytrace(pixbuffer, scene, num_objects, width, height);
 
-  // Write a PPM based on pixel buffer data
-  ppm_output(pixbuffer, argv[4], sizeof(Pixel)*(width*height), 255, width, height);
-  return 0;
+	// Write a PPM based on pixel buffer data
+	ppm_output(pixbuffer, argv[4], sizeof(Pixel)*(width*height), 255, width, height);
+	return 0;
 }
 
 int read_scene(char* json_input, Scene scene){
@@ -370,8 +364,8 @@ int read_scene(char* json_input, Scene scene){
 }
 
 // Main raycast method that will check for collisions and populate pixels into a buffer
-int raycast(Pixel *buffer, Scene scene, int num_objects, int width, int height){
-	printf("raycast entered\n");
+int raytrace(Pixel *buffer, Scene scene, int num_objects, int width, int height){
+	printf("raytrace entered\n");
 	// Initialize camera and its values
 	double cx = 0;
 	double cy = 0;
@@ -588,8 +582,8 @@ double plane_intersection(double* Ro, double* Rd, double* c, double *n){
 	double f = v3_dot(Rd, n);
 
 	double t = -(e + d)/f;
-  if (t > 0){ return t; }
-  return -1;
+	if (t > 0){ return t; }
+	return -1;
 }
 
 void shoot_ray(Object *scene, int num_objects, double *Ro, double *Rd, double *t, int *o){
